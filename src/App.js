45d273da096ba1dd0464b1f44e96c60704c5839c
nandhuganesan <nandhuganesum@gmail.com>
2021-06-02 08:33:43 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import Expense from "./components/Expenses/Expense";
+const App=()=> {
+  const expenseItem = [
+    {
+      id: "e1",
+      title: "Mobile recharge",
+      amount: 400.0,
+      date: new Date(2021, 4, 25),
+    },
+    {
+      id: "e2",
+      title: "Manga",
+      amount: 800.0,
+      date: new Date(2021, 6, 23),
+    },
+    {
+      id: "e3",
+      title: "Game CD",
+      amount: 2400.0,
+      date: new Date(2021, 10, 5),
+    },
+    {
+      id: "e3",
+      title: "OTT subscription",
+      amount: 500.0,
+      date: new Date(2021, 9, 2),
+    },
+  ];
+  return (
+    <div>
+      <Expense items={expenseItem} />
+    </div>
+  );
+}
+
+export default App;
